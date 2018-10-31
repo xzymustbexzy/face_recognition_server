e9fb1cff1e42a29f6d4346e88c16e17da076892f
Sword_holder <925937463@qq.com>
2018-10-31 09:50:39 +0800

    Add some background pages

---
+++ faceService/static/js/admin.js
@@ -0,0 +1,26 @@
+function setParameters()
+{
+    var main_title = $('#main_title');
+    main_title.text('参数设置');
+    var info_list = $('#info_list');
+    info_list.html(
+        '<li>\
+            <lable>图片默认存放路径：</lable>\
+            <span>/faceService/image</span>\
+        </li>');
+}
+
+function loginedFace()
+{
+    alert('loginedFace');
+}
+
+function chekcLog()
+{
+    alert('checkLog');
+}
+
+function image()
+{
+    alert('image');
+}
