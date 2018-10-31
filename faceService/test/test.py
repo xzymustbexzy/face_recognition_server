1eb561bcd38d16dde38314ce682321a85124380d
Sword_holder <925937463@qq.com>
2018-10-31 11:25:12 +0800

    Add a tiny hint

--- faceService/test/test.py
+++ faceService/test/test.py
@@ -1,7 +1,7 @@
 from API import add_face, check_person, parse_result
 import os
 
-def add_all_faces(file_folder):
+def batch_login(file_folder):
     file_folder_path = os.getcwd() + '/faceService/test/' + file_folder + '/'
     image_name_list = os.listdir(file_folder_path)
     uid_list = [os.path.splitext(fullname)[0] for fullname in image_name_list]
@@ -9,7 +9,8 @@ def add_all_faces(file_folder):
         res = add_face(uid, image_name_list)
         code = parse_result(res, 'code')
         assert (code == 0) # 断言添加成功
+    print('已成功添加' + str(len(uid_list)) + '个用户！')
 
 
 if __name__ == '__main__':
-    add_all_faces('login_image') #批量添加人脸
+    batch_login('login_image') #批量添加人脸
