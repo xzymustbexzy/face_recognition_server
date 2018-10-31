683929d39b53c6904e97619ecf1cbe1e23696228
Ziyang Xiao <925937463@qq.com>
2018-10-30 19:14:54 -0700

    Ready to implement test

--- faceService/__init__.py
+++ faceService/__init__.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 
 
 # 加载配置文件
-app.config.from_object('server.config')     #模块下的config文件名，不用加py后缀
+app.config.from_object('faceService.config')     #模块下的config文件名，不用加py后缀
 app.config.from_envvar('FLASK_SETTINGS')   #环境变量，指向配置文件setting的路径
 # ubuntu: export FLASK_SETTINGS=config.py
 
 NUMBER_OF_FEATURE = 0
 tolerance = 0
-with open('server/parameters.json', 'r') as parameters:
+with open('faceService/parameters.json', 'r') as parameters:
     parameters_dic = json.load(parameters)
 
 '''parameters variable'''
