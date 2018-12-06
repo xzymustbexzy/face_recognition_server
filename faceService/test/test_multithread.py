360b36ae5143d293a053631c89dde18555d6acb2
Sword_holder <925937463@qq.com>
2018-12-06 11:03:31 +0800

    Add test procedure

---
+++ faceService/test/test_multithread.py
@@ -0,0 +1,37 @@
+from API import add_face, check_person, parse_result
+import os
+import json
+import threading
+
+class login_agent(threading.Thread):
+    def __init__(self, threadID, name, file_folder):
+        self.threadID = threadID
+        self.name = name
+        self.file_folder = file_folder
+    def run(self):
+        print ("开始线程：" + self.name)
+        self.multithread_login()
+        print ("退出线程：" + self.name)
+
+    def multithread_login(self):
+        file_folder_path = os.getcwd() + '/' + self.file_folder
+        image_name_list = os.listdir(file_folder_path)
+        uid_list = [os.path.splitext(fullname)[0] for fullname in image_name_list]
+        for i in range(len(uid_list)):
+            res = add_face(uid_list[i], self.file_folder + image_name_list[i])
+            code = parse_result(res, 'code')
+            assert (code == 0) # 断言添加成功
+            print('成功添加用户' + uid_list[i])
+        print('已成功添加' + str(len(uid_list)) + '个用户！')
+        return uid_list
+
+if __name__ == '__main__':
+    thread0 = login_agent(0, '线程0', 'login_face/batch_01/')
+    thread1 = login_agent(1, '线程1', 'login_face/batch_02/')
+    thread2 = login_agent(2, '线程2', 'login_face/batch_03/')
+    thread3 = login_agent(3, '线程3', 'login_face/batch_04/')
+    thread0.join()
+    thread1.join()
+    thread2.join()
+    thread3.join()
+    print('批量注册完成！')
